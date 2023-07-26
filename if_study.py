cf1ef1663824d7c22a04814090c68f1aa0e010e2
skypark5205 <skypark5205@naver.com>
2023-07-27 01:55:31 +0900

    list,if

---
+++ if_study.py
@@ -0,0 +1,12 @@
+study = int(input('study : '))
+
+if study>=90:
+    print('A:{}'.format(study))
+elif study>=80:
+    print('B:{}'.format(study))
+elif study>=70:
+    print('C:{}'.format(study))
+elif study>=60:
+    print('D:{}'.format(study))
+else :
+    print('F')
---
+++ list.py
@@ -0,0 +1,6 @@
+list = ['한', '중', '베']
+
+list.append('일')
+
+print(list)
+print(list[0])
